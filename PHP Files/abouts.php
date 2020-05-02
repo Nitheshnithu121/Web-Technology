03bb6ffdb62fb0142c11d30fffa6b654a3865c5e
NITHESH <50882514+Nitheshnithu121@users.noreply.github.com>
2020-05-02 13:56:22 +0530

    PHP Files

---
+++ PHP Files/abouts.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AboutUs</title>
+	<link rel="stylesheet" type="text/css" href="css/abouts.css">
+   <link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+</head>
+<body>
+	 <?php include'header.php'; ?>
+
+   <div class="team-s" style="">
+   	<h1>Our Team</h1>
+   	<span class="border"></span>
+   	<div class="ps">
+   		<a href="#p1"><img src="img/ree.jpg" alt=""></a>
+   		<a href="#p2"><img src="img/ree.jpg" alt=""></a>
+   		<a href="#p3"><img src="img/ree.jpg" alt=""></a>
+   	</div>
+   	<div class="section" id="p1">
+   		<span class="name">varun</span>
+   		<span class="border"></span>
+   		<p>St.Joseph Engineering College
+   			<br>4SO17CS417
+   			<br> 4<sup>th</sup> Year
+   		    <br>Computer Science And Engineering
+   		    <br>Mangaluru</p>
+   	</div>
+
+   	<div class="section" id="p2">
+   		<span class="name">Shrinivas</span>
+   		<span class="border"></span>
+   		<p>St.Joseph Engineering College
+   			<br>4SO16CS419
+   			<br> 4<sup>th</sup> Year
+   		    <br>Computer Science And Engineering
+   		    <br>Mangaluru</p>
+   	</div>
+
+   	<div class="section" id="p3">
+   		<span class="name">Shriyankar</span>
+   		<span class="border"></span>
+   		<p>St.Joseph Engineering College
+   			<br>4SO16CS115
+   			<br> 4<sup>th</sup> Year
+   		    <br>Computer Science And Engineering
+   		    <br>Mangaluru</p>
+   	</div>
+   </div>
+
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
